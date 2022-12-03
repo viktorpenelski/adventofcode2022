67e03f527eb21afabcc8a72bef6d3d5bafc3b2f4
Victor Penelski <victor.penelski@gmail.com>
2022-12-03 07:28:25 +0200

    slightly more humane day 3

--- day_3.py
+++ day_3.py
@@ -1,9 +1,5 @@
-inputs = []
 with open('inputs/day_3.txt', 'r') as f:
     inputs = [row.rstrip() for row in f.readlines()]
-    for row in f.readlines():
-        inputs = [r.r]
-        rstrip = row.rstrip()
 
 
 def calculate_priority(char: str):
@@ -11,25 +7,32 @@ def calculate_priority(char: str):
         return 1 + (ord(char) - ord('a'))
     return 27 + (ord(char) - ord('A'))
 
+
+def common_items_from_bag(elf_bag: str):
+    left = set(elf_bag[0:len(elf_bag) // 2])
+    right = set(elf_bag[len(elf_bag) // 2:len(elf_bag)])
+    return left.intersection(right)
+
+
 def part_one():
     sum_prios = 0
     for elf_bag in inputs:
-        prio = 0
-        left = set(elf_bag[0:len(elf_bag)//2])
-        right = set(elf_bag[len(elf_bag)//2:len(elf_bag)])
-        common = left.intersection(right)
-        for item in common:
-            prio += calculate_priority(item)
+        common = common_items_from_bag(elf_bag)
+        prio = sum([calculate_priority(item) for item in common])
         sum_prios += prio
     return sum_prios
 
-# badge_prio = 0
-# for i in range(0, len(inputs), 3):
-#     first = set(inputs[i][0] + inputs[i][1])
-#     second = set(inputs[i+1][0] + inputs[i+1][1])
-#     third = set(inputs[i+2][0] + inputs[i+2][1])
-#     badge = first.intersection(second).intersection(third)
-#     badge_prio += calculate_priority(badge.pop())
+
+def part_two():
+    badge_prio = 0
+    for i in range(0, len(inputs), 3):
+        first = set(inputs[i])
+        second = set(inputs[i+1])
+        third = set(inputs[i+2])
+        badge = first.intersection(second).intersection(third)
+        badge_prio += calculate_priority(badge.pop())
+    return badge_prio
 
 
 print(part_one())
+print(part_two())
