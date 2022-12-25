e7c7ff6935d3fcd989261c834e03875a76ead691
Victor Penelski <victor.penelski@gmail.com>
2022-12-25 18:10:23 +0200

    add aocd to automate inputs downloading

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from aocd import get_data
 
 
-# Press the green button in the gutter to run the script.
+def download_year(year: int, overwrite: bool = False):
+    for day in range(1, 26):
+        file_name = f'inputs/day_{day}.txt'
+        if os.path.exists(file_name) and not overwrite:
+            continue
+        data = get_data(year=year, day=day)
+        with open(file_name, 'w') as f:
+            f.write(data)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    download_year(2022, True)
